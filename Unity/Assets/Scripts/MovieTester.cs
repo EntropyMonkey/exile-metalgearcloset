d9b9d2d8d555d08c37a9861b0e1e117312085ee2
Andrea Distler <andrea.distler@gmail.com>
2013-11-01 09:42:10 +0100

    added basic unity project

---
+++ Unity/Assets/Scripts/MovieTester.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovieTester : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			MovieTexture t = renderer.material.mainTexture as MovieTexture;
+			if (t.isPlaying)
+				t.Pause();
+			else
+				t.Play();
+		}
+	}
+}
