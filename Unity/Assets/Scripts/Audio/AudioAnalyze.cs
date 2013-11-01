e5d828a530f21cb52ff9277ac41d9c8d9d13ae14
trondtrond <trond@glomnes.no>
2013-11-01 23:28:04 +0100

    added audio analyzing

--- Unity/Assets/Scripts/Audio/AudioAnalyze.cs
+++ Unity/Assets/Scripts/Audio/AudioAnalyze.cs
@@ -1,9 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class AudioAnalyze : MonoBehaviour {
 	
 	int numSamples = 2048;
+	
+	Queue<float> soundQueue = new Queue<float>();
+	public int numFrames = 30;
+	float soundSum = 0;
+	
+	public int minFreq = 0;
+	public int maxFreq = 15;
 	
 	// Use this for initialization
 	void Start () {
@@ -12,16 +20,17 @@ public class AudioAnalyze : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		    
+		SoundAmplitude();
+		//print (GetAvgSound());
 		
-		FindFrequenzy();
+		//FindFrequenzy();
 	}
 	
 	void FindFrequenzy(){
 		
        float[] specter =  AudioListener.GetSpectrumData(numSamples, 0,FFTWindow.BlackmanHarris);
         
-		int pos = 10;
+		int pos = 15;
 		print("Freq: "+pos*AudioSettings.outputSampleRate/numSamples+"   strength "+Mathf.Abs(specter[pos]));
 		
 		/*
@@ -39,6 +48,35 @@ public class AudioAnalyze : MonoBehaviour {
         	*/
 		
 
+    }
+	
+	public float GetAvgSound(){
+		return (soundSum/numFrames)*10000000;	
+	}
+	
+	 void SoundAmplitude(){
+		float[] specter =  AudioListener.GetSpectrumData(numSamples, 0,FFTWindow.BlackmanHarris);
+		
+		float totalSound = 0;
+		
+		for(int i=minFreq; i < maxFreq; i++)
+        {
+              
+             totalSound += Mathf.Abs(specter[i]);
+             
+               
         }	
+		
+		float avgSound = totalSound/numSamples;
+		soundQueue.Enqueue(avgSound);
+		soundSum += avgSound;
+		if(soundQueue.Count > numFrames){
+			soundSum -= soundQueue.Dequeue();
+		}
+			
+		
+		
+	}
+		
 	
 }
---
+++ Unity/Assets/Scripts/TestScripts/KeepQuiet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeepQuiet : MonoBehaviour {
+	
+	public float detectionVolume = 100;
+	
+	int detectedNum = 0;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(GetComponent<AudioAnalyze>().GetAvgSound() > detectionVolume){
+			detectedNum++;
+			print("DETECTED!!! "+detectedNum);
+			
+			
+		}
+	}
+}
