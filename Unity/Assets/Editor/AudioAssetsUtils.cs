e793218a6ee104fc277e32d7b187f0284a7c471e
vincerp <vincependrell@gmail.com>
2014-03-01 16:36:41 +0100

    rock the casbah with editor shit

---
+++ Unity/Assets/Editor/AudioAssetsUtils.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class AudioAssetsUtils {
+
+	[MenuItem("Assets/Audio/Set Audio Files as 2D")]
+	public static void SetAudioFilesAs2D(){
+		Debug.Log(Selection.activeObject.GetType());
+		Object[] audios = Selection.GetFiltered(typeof(AudioClip), SelectionMode.Assets);
+
+		int amount = 0;
+		foreach(AudioClip a in audios){
+			string path = AssetDatabase.GetAssetPath(a);
+			AudioImporter ai = AudioImporter.GetAtPath(path) as AudioImporter;
+
+			if(ai.threeD){
+				ai.threeD = false;
+				amount++;
+			}
+		}
+
+		Debug.Log(""+amount+" AudioClips converted to 2D.");
+	}
+	
+	[MenuItem("Assets/Audio/Set Audio Files as 3D")]
+	public static void SetAudioFilesAs3D(){
+		Debug.Log(Selection.activeObject.GetType());
+		Object[] audios = Selection.GetFiltered(typeof(AudioClip), SelectionMode.Assets);
+		
+		int amount = 0;
+		foreach(AudioClip a in audios){
+			string path = AssetDatabase.GetAssetPath(a);
+			AudioImporter ai = AudioImporter.GetAtPath(path) as AudioImporter;
+			
+			if(!ai.threeD){
+				ai.threeD = true;
+				amount++;
+			}
+		}
+		
+		Debug.Log(""+amount+" AudioClips converted to 3D.");
+	}
+}
